6add56a5658dcb342dd967f53471d2b38535e26c
Albert Khomich <khomich1022@gmail.com>
2024-02-09 13:55:00 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello Bro!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
